58cddbab6df97602a64bc13edcff441b014a7427
anugrahiman5 <anugrah.iman.5@gmail.com>
2020-07-27 11:18:52 +0700

    membuat adapter

---
+++ app/src/main/java/com/anugrah/firestorerecyclerview/MainActivity.java
@@ -0,0 +1,85 @@
+package com.anugrah.firestorerecyclerview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore firebaseFirestore;
+    private RecyclerView mFirestoreList;
+
+    private FirestoreRecyclerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        mFirestoreList = findViewById(R.id.firestore_list);
+
+        //Query
+        Query query = firebaseFirestore.collection("Products");
+        //RecyclerOptions
+        FirestoreRecyclerOptions<ProductsModel> options = new FirestoreRecyclerOptions.Builder<ProductsModel>()
+                .setQuery(query, ProductsModel.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<ProductsModel, ProductsViewHolder>(options) {
+            @NonNull
+            @Override
+            public ProductsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_single, parent, false);
+                return new ProductsViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ProductsViewHolder holder, int position, @NonNull ProductsModel model) {
+                holder.list_name.setText(model.getName());
+                holder.list_price.setText(model.getPrice() + "");
+            }
+        };
+
+//        mFirestoreList.setHasFixedSize(true);
+        mFirestoreList.setLayoutManager(new LinearLayoutManager(this));
+        mFirestoreList.setAdapter(adapter);
+
+    }
+
+    private class ProductsViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView list_name, list_price;
+
+        public ProductsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            list_name = itemView.findViewById(R.id.list_name);
+            list_price = itemView.findViewById(R.id.list_price);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+}
